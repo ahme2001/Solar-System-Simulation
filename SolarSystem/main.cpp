972ce30edc04c2775b02c5c087da51ce819faa96
ahme2001 <ahmedaboeleid30@gmail.com>
2023-05-09 23:46:58 +0300

    collision is done

--- SolarSystem/main.cpp
+++ SolarSystem/main.cpp
@@ -7,10 +7,24 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 #include <glm/vec3.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
 #include <cstdint>  // for uintptr_t
 
 // Globals.
 std::vector<glm::vec3> points = {
+glm::vec3(0.0, 0.0, 0.0),  // sun
+glm::vec3(0.0 ,0.0, 40.0), // mercury
+glm::vec3(0.0, 0.0, 50.0),  // venus
+glm::vec3(0.0, 0.0, 62.0), // earth
+glm::vec3(0.0, 10.0, 62.0), // moon
+glm::vec3(0.0, 0.0, 75.0),  // mars
+glm::vec3(0.0, 0.0, 100.0), // jupiter
+glm::vec3(0.0, 0.0, 140.0),  // saturn
+glm::vec3(0.0, 0.0, 170.0), // uranus
+glm::vec3(0.0, 0.0, 190.0)  // neptune
+};
+std::vector<glm::vec3> newPosition = {
 glm::vec3(0.0, 0.0, 0.0),  // sun
 glm::vec3(0.0, 0.0, 40.0), // mercury
 glm::vec3(0.0, 0.0, 50.0),  // venus
@@ -34,21 +48,18 @@ GLfloat solar_diffuses[][4] = {
 {0.6f, 0.9f, 0.9f, 1.0},
 {0.2f, 0.4f, 1.0f, 1.0}};
 GLfloat rotation[] = {0.0,1.59,1.17,1.0,1.0,0.8,0.46,0.33,0.228,0.182};
-std::vector<GLfloat> sizePlanet = {5.0,0.4,0.9,1.0,0.25,0.6,3.0,2.8,1.25,1.2};
+std::vector<GLfloat> sizePlanet = {5.0,0.4,0.9,1.0,0.25,0.6,3.0,2.5,1.25,1.2};
 
+static uintptr_t font = reinterpret_cast<uintptr_t>(GLUT_BITMAP_8_BY_13); // Font selection
 static int width, height; // Size of the OpenGL window.
 static float latAngle = 0.0; // Latitudinal angle.
 static int isAnimate = 0; // Animated?
+static int isCollision = 0;
 static int animationPeriod = 100; // Time interval between frames.
 static float angle = 90.0 , xVal = 220, zVal = 0; // Angle & Co-ordinates of the spacecraft.
 static unsigned int spacecraft; // Display lists base index.
 static unsigned int sphere;
 
-// lighting
-static float d = 3.0; // Diffuse and specular white light intensity.
-static int localViewer = 1; // Local viewpoint?
-static float p = 1.0; // Positional light?
-static float t = 0.0; // Quadratic attenuation factor.
 // Initialization routine.
 void setup(void)
 {
@@ -84,12 +95,39 @@ void setup(void)
 	glEnable(GL_CULL_FACE);
 	glCullFace(GL_BACK);
 }
+// Routine to draw a bitmap character string.
+void writeBitmapString(void *font, char *string)
+{
+	char *c;
+
+	for (c = string; *c != '\0'; c++) glutBitmapCharacter(font, *c);
+}
+
+int checkSpheresIntersection(float x1, float y1, float z1, float r1,
+	float x2, float y2, float z2, float r2)
+{
+	return ((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2) + (z1 - z2)*(z1 - z2) <= (r1 + r2)*(r1 + r2));
+}
+
+// Function to check if the spacecraft collides with an asteroid when the center of the base
+// of the craft is at (x, 0, z) and it is aligned at an angle a to to the -z direction.
+// Collision detection is approximate as instead of the spacecraft we use a bounding sphere.
+int asteroidCraftCollision(float x, float z, float a)
+{
+	int i;
+	// Check for collision with each asteroid.
+    for (i = 0; i<sizePlanet.size(); i++)
+            if (checkSpheresIntersection(x - 5 * sin((M_PI / 180.0) * a), 0.0,
+                z - 5 * cos((M_PI / 180.0) * a), 7.072,
+                newPosition[i].x, newPosition[i].y,
+                newPosition[i].z, 6 * sizePlanet[i]))
+                return 1;
+	return 0;
+}
 
 void drawingSystem(void){
     // sun
     glColor3f(1.0, 1.0, 0.0);
-    glMaterialfv(GL_FRONT, GL_AMBIENT, solar_diffuses[0]);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, solar_diffuses[0]);
     glPushMatrix();
     glTranslatef(points[0].x,points[0].y,points[0].z);
     glScalef(sizePlanet[0],sizePlanet[0],sizePlanet[0]);
@@ -98,20 +136,23 @@ void drawingSystem(void){
     glEnable(GL_LIGHTING);
     // draw planets and moon
     for(int i=1;i<10;i++){
-        glMaterialfv(GL_FRONT, GL_AMBIENT, solar_diffuses[i]);
-        glMaterialfv(GL_FRONT, GL_DIFFUSE, solar_diffuses[i]);
+        glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, solar_diffuses[i]);
         glPushMatrix();
         glRotatef(latAngle*rotation[i], 0.0, 1.0, 0.0);
         if(i == 4) glRotatef(50*latAngle, 0.0, 0.0, 1.0); // control rotation of moon around earth
         glTranslatef(points[i].x,points[i].y,points[i].z);
         glScalef(sizePlanet[i],sizePlanet[i],sizePlanet[i]);
         glCallList(sphere);
-        if(i == 7){// draw ring around saturn
+        if(i == 7){ // draw ring around saturn
             glColor3f(0.0, 1.0, 0.0);
-            glRotatef(40.0,1.0,0.0,0.0);
-            glutSolidTorus(1.0, 7.0, 5, 20);
+            glRotatef(30.0,1.0,0.0,0.0);
+            glutSolidTorus(0.5, 7.0, 5, 30);
         }
         glPopMatrix();
+        float newX = points[i].x * cos(glm::radians(latAngle*rotation[i])) + points[i].z * sin(glm::radians(latAngle*rotation[i]));
+        float newY = points[i].y;
+        float newZ = -points[i].x * sin(glm::radians(latAngle*rotation[i])) + points[i].z * cos(glm::radians(latAngle*rotation[i]));
+        newPosition[i] = glm::vec3(newX, newY, newZ);
     }
 }
 
@@ -119,19 +160,25 @@ void drawingSystem(void){
 void drawScene(void)
 {
     float lightAmb[] = { 0.0, 0.0, 0.0, 1.0 };
-	float lightDifAndSpec0[] = { d, d, d, 1.0 };
-	float lightPos0[] = { 0.0, 0.0, 0.0, p };
+	float lightDifAndSpec0[] = { 2.0, 2.0, 2.0, 1.0 };
+	float lightPos0[] = { 0.0, 0.0, 0.0, 1.0 };
 	glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmb);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDifAndSpec0);
 	glLightfv(GL_LIGHT0, GL_SPECULAR, lightDifAndSpec0);
-	glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, localViewer); // Enable local viewpoint
+	glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, 1); // Enable local viewpoint
 	glEnable(GL_LIGHT0);
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	// Begin right viewport.
+	// Begin whole viewport.
 	glViewport(0, 0, width , height);
-    glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, t);
+    glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, 0.0);
 	glLoadIdentity();
+	// Write text in isolated (i.e., before gluLookAt) translate block.
+    glPushMatrix();
+    glColor3f(1.0, 0.0, 0.0);
+    glRasterPos3f(-28.0, 25.0, -30.0);
+    if (isCollision) writeBitmapString((void*)font, "Game Over, Start again!");
+    glPopMatrix();
     // Fixed camera.
     gluLookAt(xVal - 10 * sin((M_PI / 180.0) * angle),
 		0.0,
@@ -150,7 +197,7 @@ void drawScene(void)
 
 	// Begin plane viewport.
     glViewport(3.0 * width / 4.0, 0, width / 3.0, height/3.0);
-    glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, t);
+    glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, 0.0);
     glLoadIdentity();
 
     // separate two views
@@ -200,38 +247,6 @@ void keyInput(unsigned char key, int x, int y)
 {
 	switch (key)
 	{
-    case 'p':
-		if (p) p = 0.0; else p = 1.0;
-		glutPostRedisplay();
-		break;
-	case 'P':
-		if (p) p = 0.0; else p = 1.0;
-		glutPostRedisplay();
-		break;
-	case 'd':
-		if (d > 0.0) d -= 0.05;
-		glutPostRedisplay();
-		break;
-	case 'D':
-		if (d < 3.0) d += 0.05;
-		glutPostRedisplay();
-		break;
-    case 'l':
-		if (localViewer) localViewer = 0; else localViewer = 1;
-		glutPostRedisplay();
-		break;
-	case 'L':
-		if (localViewer) localViewer = 0; else localViewer = 1;
-		glutPostRedisplay();
-		break;
-    case 't':
-		if (t > 0.0) t -= 0.01;
-		glutPostRedisplay();
-		break;
-	case 'T':
-		t += 0.01;
-		glutPostRedisplay();
-		break;
     case ' ':
 		if (isAnimate) isAnimate = 0;
 		else
@@ -271,12 +286,18 @@ void specialKeyInput(int key, int x, int y)
 	if (tempAngle < 0.0) tempAngle += 360.0;
 
 	// Move spacecraft to next position only if there will not be collision with an asteroid.
-	//if (!asteroidCraftCollision(tempxVal, tempzVal, tempAngle))
-	//{
+	if (!asteroidCraftCollision(tempxVal, tempzVal, tempAngle))
+	{
+	    isCollision = 0;
 		xVal = tempxVal;
 		zVal = tempzVal;
 		angle = tempAngle;
-	//}
+	}else{
+	    isCollision = 1;
+        xVal = 200;
+        zVal = 0;
+        angle = 90;
+	}
 
 	glutPostRedisplay();
 }
